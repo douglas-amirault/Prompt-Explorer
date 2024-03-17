f708607924b64388ebe0bcb6c70d70171f27a89c
Amirault, Douglas Richard <damirault6@gatech.edu>
2024-03-17 18:06:55 -0400

    back to only 5k images

--- get_data.py
+++ get_data.py
@@ -1,5 +1,5 @@
 from src.data_loader import DataLoader
 
-dl = DataLoader("poloclub/diffusiondb", "2m_all")
+dl = DataLoader("poloclub/diffusiondb", "2m_first_5k")
 
-dl.download_dataset(2_000_000)
+dl.download_dataset(5000)
