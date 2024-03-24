69ee7362caed0b64193eb0e1211bc14b75804751
Douglas Amirault <douglas.amirault@gmail.com>
2024-03-24 16:28:54 -0400

    added nltk requirements

--- get_data.py
+++ get_data.py
@@ -1,5 +1,9 @@
+import nltk
 from src.data_loader import DataLoader
 
 dl = DataLoader("poloclub/diffusiondb", "2m_first_5k")
 
 dl.download_dataset(5000)
+
+nltk.download("punkt")
+nltk.download("averaged_perceptron_tagger")
